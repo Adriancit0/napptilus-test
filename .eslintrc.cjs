ed3d742043930880c7a1cfe92c3c336fb4c1dec2
adrian.ruano <adrianruanol@gmail.com>
2022-10-18 22:21:14 +0200

    feat(web): Setup project

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'eslint-config-prettier',
+    'standard'
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off'
+  }
+}
